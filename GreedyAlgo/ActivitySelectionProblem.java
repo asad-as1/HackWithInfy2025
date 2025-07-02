88a67a233fb340d92b2b902cceaea734500f76b2
Mohd Asad Ansari <asad.as9458@gmail.com>
2025-07-02 09:34:10 +0530

    sd

--- GreedyAlgo/ActivitySelectionProblem.java
+++ GreedyAlgo/ActivitySelectionProblem.java
@@ -14,7 +14,7 @@ public class ActivitySelectionProblem {
     
     Arrays.sort(activities, (a, b) -> a[1] - b[1]);
     int count = 1;
-    int lastEnd = activities[0][0];
+    int lastEnd = activities[0][1];
     for (int i = 1; i < activities.length; i++) {
       if (activities[i][0] >= lastEnd) {
         count++;
