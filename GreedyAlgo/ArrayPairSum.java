d8ec990ad741d8893de103559b454432c8c9e472
Mohd Asad Ansari <asad.as9458@gmail.com>
2025-07-03 09:39:19 +0530

    array pair sum

---
+++ GreedyAlgo/ArrayPairSum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ArrayPairSum {
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            maxSum += nums[i];
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = {1, 4, 3, 2};
+        System.out.println(arrayPairSum(nums1)); 
+    }
+}
+
